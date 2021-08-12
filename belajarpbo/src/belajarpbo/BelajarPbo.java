1079f3c36ae6d537a85df9dd6d802d3276c7e48d
Nurul Mustofa <66587167+nioke-dev@users.noreply.github.com>
2021-08-13 05:30:14 +0700

    java perhitungan bangun datar

---
+++ belajarpbo/src/belajarpbo/BelajarPbo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajarpbo;
+
+/**
+ *
+ * @author NIOKE
+ */
+public class BelajarPbo {
+    public static void main(String[] args) {
+        System.out.println("-----DATA DIRI-----");
+        System.out.println("Nama   : Nurul Mustofa");
+        System.out.println("Kelas  : XI RPL 2");        
+        System.out.println("ALamat : Banyuanyar Lor");
+        System.out.println("No WA  : 085608969639");
+    }
+}
---
+++ belajarpbo/src/belajarpbo/LuasPersegi.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajarpbo;
+
+/**
+ *
+ * @author NIOKE
+ */
+public class LuasPersegi {
+    public static void main(String[] args) {        
+        int Sisi = 8;
+        int Hasil = Sisi * Sisi;
+        
+        System.out.println("sisi = " + Sisi);
+        System.out.println("Jadi Luas Persegi = " + Hasil);
+        
+    }
+    
+}
